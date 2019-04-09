e180b1bf9c9700c0d203a48bd6c7db33ccf7001a
albin143 <albinthomasmolly84@gmail.com>
2019-04-09 10:42:47 +0530

    template engine rendering handlebars

---
+++ nodejs/hndle.js
@@ -0,0 +1,13 @@
+const express=require('express'); //loading the express
+const exph=require('express-handlebars');
+
+var hndle=express();// the express is must use the function name
+hndle.engine('handlebars',exph());
+hndle.set('view engine','handlebars');
+hndle.get('/',(req,res)=>{
+    res.render("index");
+});
+hndle.get('/about',(req,res)=>{
+    res.render("about")
+})
+hndle.listen(3500);
--- nodejs/json.js
+++ nodejs/json.js
@@ -3,9 +3,6 @@ const express=require('express'); //loading the express
 
 var app=express();// the express is must use the function name
 app.get('/',(req,res)=>{
-    res.send("<marquee><h1> വെല്‍കം റ്റൂ ഊട്ടി നൈസ് റ്റൂ മീറ്റ് യൂ </h1></marquee>")
+    res.send('[{"name":"tom","age":23},{"name":"riya","age":23]'
 }); //the route defined "get" / is the priliminary route
-app.get('/home',(req,res)=>{
-    res.send("welcom to our web site");
-});
 app.listen(3000) // 3000 is the port to run
