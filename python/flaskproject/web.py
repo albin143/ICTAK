f0662f07474b25a7e44a874b74de8d845e589e94
albin143 <albinthomasmolly84@gmail.com>
2019-04-09 16:18:10 +0530

    flask pro

--- python/flaskproject/web.py
+++ python/flaskproject/web.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask,render_templates 
 app=Flask(__name__)
-@app.route("/")
-def greeting():
-	return"<marquee><h1 style='color:red;'>hello world!yooo yoo</h1></marquee>"
-if __name__=="__main__":
-	app.run(host='127.0.0.1')
 
+
+@app.route("/")
+def index():
+	return render_template('index.html')
+
+
+if(__name__=="__main__"):
+	app.run(debug=true)
+ 
