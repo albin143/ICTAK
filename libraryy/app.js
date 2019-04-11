912db8f86999de8bd68a09e2378dfd0c49e80d02
albin143 <albinthomasmolly84@gmail.com>
2019-04-11 15:04:56 +0530

    eja1s

--- libraryy/app.js
+++ libraryy/app.js
@@ -7,13 +7,14 @@ var app= express();//object creation
 app.set('views','./src/views');
 app.set('view engine','ejs');
 app.get('/',function(req,res){
-    res.render('index');
-});
+    res.render('index',{list:['book1','book2','book3'],
+      title:"Library"});
 
+});
 
 
 // app.get('/',(req,res)=>{
 //     res.send('hello from my library');
 app.listen(3000,function(){
-    console.log('listening to port'+chalk.green('3000'));
+    console.log('listening to port'+chalk.red('3000'));
 });
